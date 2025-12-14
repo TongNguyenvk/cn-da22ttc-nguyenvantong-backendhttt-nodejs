760fc312057540766af516773797174c73dffc5f
TongNguyenvk <tongct08@gmail.com>
2025-12-14 15:24:02 +0700

    Remove unnecessary test scripts, debug files, and documentation files

--- migrations/check_constraints.sql
+++
@@ -1,7 +0,0 @@
--- Check unlock_type values
-SELECT DISTINCT unlock_type FROM "Avatars";
-
--- Check constraints
-SELECT conname, pg_get_constraintdef(oid) 
-FROM pg_constraint 
-WHERE conrelid = '"Avatars"'::regclass AND contype = 'c';
